c8b0df224ecadec1c8073dfa13efc3d3b3177c45
Shaun <hajishahshaun@gmail.com>
2021-05-06 13:29:04 -0700

    Update messages

--- app/src/main/java/com/example/petnetwork/fragments/MessageFragment.java
+++ app/src/main/java/com/example/petnetwork/fragments/MessageFragment.java
@@ -4,13 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.example.petnetwork.PostsAdapter;
 import com.example.petnetwork.R;
+
+import java.util.ArrayList;
 
 public class MessageFragment extends Fragment {
 
+    private TextView tvMessage;
 
 
 
@@ -26,8 +35,21 @@ public class MessageFragment extends Fragment {
     }
 
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        tvMessage = view.findViewById(R.id.tvMessage);
 
 
+    }
+
+
+    protected void queryMessages() {
+
+
+
+    }
 
 
 }
